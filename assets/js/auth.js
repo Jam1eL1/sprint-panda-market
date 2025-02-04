b4bf3db7231a557338bae3a1bf4177e4dacd2191
Jam1eL1 <23jisu@gmail.com>
2025-02-04 14:23:26 +0900

    fix: modify handling  .auth__form-password-toggle with js for code efficiency

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,9 +1,9 @@
-function togglePassword() {
-  var input = document.getElementById("password-confirmation");
-  console.log(`${input.value}`);
-  if (input.type === "password") {
-    input.type = "text";
-  } else {
-    input.type = "password";
+// handling aut__form-password-toggle for password input visibility 
+document.querySelectorAll(".auth__form-password-toggle").forEach((btn) => {
+btn.addEventListener("click", ()=>{
+  const userInput = btn.previousElementSibling;
+  if (userInput && userInput.classList.contains("auth__form-password-input")) {
+    userInput.type = userInput.type === "password"? "text" : "password";
   }
-}
+});
+});
